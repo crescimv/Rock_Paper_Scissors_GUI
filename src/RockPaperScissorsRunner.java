611d5e3210fabe95ee6059a02b6cf647e0422286
crescimv <crescimv@mail.uc.edu>
2024-10-13 14:16:43 -0400

    Initial commit

---
+++ src/RockPaperScissorsRunner.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.lang.annotation.Inherited;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame RockPaperScissorsFrame = new InheritedFrame();
+    }
+}
